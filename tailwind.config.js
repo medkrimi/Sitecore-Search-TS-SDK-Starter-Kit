f2636c9ed5a9386a505d56b04f6d3622c4dc7145
Mohamed Krimi <contact@medkrimi.net>
2024-12-13 20:28:51 -0500

    Fix Tailwind config to deploy into Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
+  type: "commonjs",
   darkMode: ['class'],
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
   prefix: '',
